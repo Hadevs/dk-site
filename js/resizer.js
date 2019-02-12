8d3c5aa719287cf448785aed3779142570279874
Alexandr Romanov <rmnff.dev@yandex.ru>
2019-02-12 15:51:40 +0300

    content tag is divided into 2 columns

---
+++ js/resizer.js
@@ -0,0 +1,22 @@
+var resize = function() {
+  var w = window.innerWidth;
+  var mobile = w <= 1000;
+  if(!mobile) {
+    document.getElementById('content').style.width = '70%';
+    document.getElementById('content').style.marginLeft = '15%';
+    document.getElementById('title').style.width = '50%';
+    document.getElementById('title').style.marginLeft = '25%';
+    document.getElementById('footer').style.width = '50%';
+    document.getElementById('footer').style.marginLeft = '25%';
+    return;
+  }
+  document.body.style.fontSize = '22pt';
+  document.getElementById('header').style.borderBottom = '3px solid';
+  document.getElementById('content').style.width = '92%';
+  document.getElementById('content').style.marginLeft = '4%';
+  document.getElementById('title').style.fontSize = '30pt';
+  document.getElementById('title').style.width = '92%';
+  document.getElementById('title').style.marginLeft = '4%';
+  document.getElementById('footer').style.width = '92%';
+  document.getElementById('footer').style.marginLeft = '4%';
+};
